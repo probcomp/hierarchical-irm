8c74e4fae425a52cb959abfff4ede28c0dc82d42
Thomas Colthurst <thomaswc@google.com>
2024-06-11 19:49:15 +0000

    Add tests for BitFlip

---
+++ cxx/emissions/bitflip_test.cc
@@ -0,0 +1,33 @@
+// Apache License, Version 2.0, refer to LICENSE.txt
+
+#define BOOST_TEST_MODULE test BitFlip
+
+#include <random>
+
+#include "emissions/bitflip.hh"
+
+#include <boost/test/included/unit_test.hpp>
+
+BOOST_AUTO_TEST_CASE(test_simple) {
+  BitFlip bf;
+
+  BOOST_TEST(bf.logp_score() == 0.0);
+  BOOST_TEST(bf.N == 0);
+  bf.incorporate(std::make_pair<bool, bool>(true, false));
+  BOOST_TEST(bf.logp_score() == 0.0);
+  BOOST_TEST(bf.N == 1);
+  bf.unincorporate(std::make_pair<bool, bool>(true, false));
+  BOOST_TEST(bf.logp_score() == 0.0);
+  BOOST_TEST(bf.N == 0);
+  bf.incorporate(std::make_pair<bool, bool>(false, true));
+  bf.incorporate(std::make_pair<bool, bool>(false, true));
+  BOOST_TEST(bf.logp_score() == 0.0);
+  BOOST_TEST(bf.N == 2);
+
+  BOOST_TEST(bf.logp(std::make_pair<bool, bool>(true, false)) == 0.0);
+
+  std::mt19937 prng;
+  BOOST_TEST(bf.sample_corrupted(false, &prng));
+
+  BOOST_TEST(bf.propose_clean({false, false, false}, &prng));
+}
---
+++ cxx/emissions/sometimes.hh
@@ -0,0 +1,78 @@
+#pragma once
+
+#include "distributions/beta_bernoulli.hh"
+#include "emissions/base.hh"
+
+// An Emission class that sometimes applies BaseEmissor and sometimes doesn't.
+// BaseEmissor must (1) of type Emission<SampleType> and (2) assign zero
+// probability to <clean, dirty> pairs with clean == dirty.  [For example,
+// BitFlip and Gaussian both satisfy #2].
+template <typename BaseEmissor, typename SampleType = double>
+class Sometimes : public Emission<SampleType> {
+ public:
+  // We use a beta distribution prior, so our hyperparameters are
+  double alpha = 1.0;
+  double beta = 1.0;
+
+  BaseEmissor be;
+  BetaBernoulli bb(nullptr);
+
+  Sometimes() {};
+
+  void incorporate(const std::pair<SampleType, SampleType>& x) {
+    ++N;
+    bb.incorporate(x.first != x.second);
+    if (x.first != x.second) {
+      be.incorporate(x);
+    }
+  }
+
+  void unincorporate(const std::pair<SampleType, SampleType>& x) {
+    --N;
+    bb.unincorporate(x.first != x.second);
+    if (x.first != x.second) {
+      be.unincorporate(x);
+    }
+  }
+
+  double logp(const std::pair<SampleType, SampleType>& x) const {
+    return bb.logp(x.first != x.second) + be.logp(x);
+  }
+
+  double logp_score() const {
+    return bb.logp_score() + be.logp_score();
+  }
+
+  void transition_hyperparameters() {
+    be.transition_hyperparameters();
+    bb.transition_hyperparameters();
+  }
+
+  double sample_corrupted(const SampleType& clean, std::mt19937* prng) {
+    bb.prng = prng;
+    if (bb.sample()) {
+      return be.sample_corrupted(clean, prng);
+    } else {
+      return clean;
+    }
+  }
+
+  SampleType propose_clean(const std::vector<SampleType>& corrupted,
+                           std::mt19937* prng) {
+    // We approximate the maximum likelihood estimate by taking the mode of
+    // corrupted.  The full solution would construct BaseEmissor and
+    // BetaBernoulli instances for each choice of clean and picking the
+    // clean with the lowest combined logp_score().
+    std::unordered_map<SampleType, int> counts;
+    SampleType mode;
+    int max_count = 0;
+    for (const SampleType& c: corrupted) {
+      ++counts[c];
+      if (counts[c] > max_count) {
+        max_count = counts[c];
+        mode = c;
+      }
+    }
+    return mode;
+  }
+};
