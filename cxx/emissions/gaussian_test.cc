d1adc767a4d861bc7743e482377c85a875885d05
Srinivas Vasudevan <srvasude@google.com>
2024-06-17 08:45:05 -0700

    Fix emissions test (merge from HEAD)

--- cxx/emissions/gaussian_test.cc
+++ cxx/emissions/gaussian_test.cc
@@ -10,7 +10,7 @@ namespace tt = boost::test_tools;
 BOOST_AUTO_TEST_CASE(simple) {
   GaussianEmission ge;
 
-  BOOST_TEST(ge.logp_score() == -0.69314718055994529, tt::tolerance(1e-6));
+  BOOST_TEST(ge.logp_score() == 0., tt::tolerance(1e-6));
   ge.incorporate(std::make_pair(1.0, 1.1));
   BOOST_TEST(ge.N == 1);
   ge.incorporate(std::make_pair(5.0, 4.9));
@@ -18,9 +18,9 @@ BOOST_AUTO_TEST_CASE(simple) {
   ge.unincorporate(std::make_pair(1.0, 1.1));
   BOOST_TEST(ge.N == 1);
 
-  BOOST_TEST(ge.logp_score() == -2.0831868619777163, tt::tolerance(1e-6));
+  BOOST_TEST(ge.logp_score() == -1.0472020831064763, tt::tolerance(1e-6));
 
-  BOOST_TEST(ge.logp(std::make_pair(2.0, 2.001)) == -0.80313245109477638,
+  BOOST_TEST(ge.logp(std::make_pair(2.0, 2.001)) == -0.80065106134957509,
              tt::tolerance(1e-6));
 
   std::mt19937 prng;
