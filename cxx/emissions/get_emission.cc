dc32973b611d6a6890737d6f105a0e493d1d5dcc
Thomas Colthurst <thomaswc@google.com>
2024-06-17 17:41:35 +0000

    Add get_emission

---
+++ cxx/emissions/get_emission.cc
@@ -0,0 +1,24 @@
+// Copyright 2024
+// See LICENSE.txt
+
+#include <cassert>
+
+#include "emissions/get_emission.hh"
+#include "emissions/bitflip.hh"
+#include "emissions/gaussian.hh"
+#include "emissions/simple_string.hh"
+
+EmissionVariant get_emission(const std::string& emission_name) {
+  if (emission_name == "gaussian") {
+    return new GaussianEmission();
+  } else if (emission_name == "simple_string") {
+    return new SimpleStringEmission();
+  } else if (emission_name == "sometimes_gaussian") {
+    return new SometimesGaussian();
+  } else if (emission_name == "sometimes_bitflip") {
+    return new SometimesBitFlip();
+  }
+  printf("Unknown emission name %s\n", emission_name.c_str());
+  assert(false);
+}
+
