a3a9dcd647ebc14b36600924e2cc5ce7ac7779aa
Srinivas Vasudevan <srvasude@google.com>
2024-07-11 15:19:49 -0700

    delete comment

--- cxx/irm_test.cc
+++ cxx/irm_test.cc
@@ -58,11 +58,15 @@ BOOST_AUTO_TEST_CASE(test_irm) {
   BOOST_TEST(r4_obs == 2);
 }
 
-BOOST_AUTO_TEST_CASE(test_irm_one_observation) {
+BOOST_AUTO_TEST_CASE(test_irm_one_data_point) {
+  // We have one data point {1, 1}, for which we compute different relations
+  // against. Because each domain (D1, D2) has only one point, there is only one
+  // cluster, and thus no marginalizing over different cluster assignments, so
+  // logp_score = logp(x).
   std::map<std::string, T_relation> schema1{
       {"R1", T_clean_relation{{"D1"}, DistributionSpec("bernoulli")}},
-      {"R2", T_clean_relation{{"D1"}, DistributionSpec("normal")}},
-      {"R3", T_clean_relation{{"D1", "D1"}, DistributionSpec("normal")}}};
+      {"R2", T_clean_relation{{"D2"}, DistributionSpec("normal")}},
+      {"R3", T_clean_relation{{"D1", "D2"}, DistributionSpec("normal")}}};
   IRM irm(schema1);
 
   BOOST_TEST(irm.logp_score() == 0.0);
@@ -78,10 +82,10 @@ BOOST_AUTO_TEST_CASE(test_irm_one_observation) {
   double logp_x = irm.logp({{"R1", {1,}, obs0}}, &prng);
   BOOST_TEST(logp_x < 0.0);
 
-  double logp_y = irm.logp({{"R2", {1,}, obs1}}, &prng);
+  double logp_y = irm.logp({{"R2", {23,}, obs1}}, &prng);
   BOOST_TEST(logp_y < 0.0);
 
-  double logp_z = irm.logp({{"R2", {1,}, obs2}}, &prng);
+  double logp_z = irm.logp({{"R3", {1, 23}, obs2}}, &prng);
   BOOST_TEST(logp_z < 0.0);
 
   // When we incorporate observations for a single domain entry, there is only
@@ -93,18 +97,13 @@ BOOST_AUTO_TEST_CASE(test_irm_one_observation) {
   BOOST_TEST(one_obs_score < 0.0);
   BOOST_TEST(one_obs_score == logp_x);
 
-  irm.incorporate(&prng, "R2", {1,}, obs1);
+  irm.incorporate(&prng, "R2", {23,}, obs1);
   double two_obs_score = irm.logp_score();
   BOOST_TEST(two_obs_score < 0.0);
   BOOST_TEST(two_obs_score == (logp_x + logp_y));
 
-  irm.incorporate(&prng, "R3", {1, 1}, obs2);
+  irm.incorporate(&prng, "R3", {1, 23}, obs2);
   double three_obs_score = irm.logp_score();
   BOOST_TEST(three_obs_score < 0.0);
   BOOST_TEST(three_obs_score == (logp_x + logp_y + logp_z));
-
-  // TODO(thomaswc):  Uncomment below when relation::unincorporate is
-  // implemented.
-  // irm.unincorporate("R1", {1,});
-  // BOOST_TEST(irm.logp_score() == 0.0);
 }
