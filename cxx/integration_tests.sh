f0c4c0c5e75b72952dc3c310574608f2340ba3f3
Thomas Colthurst <thomaswc@google.com>
2024-08-07 13:32:51 +0000

    Fix string alignment bug

--- cxx/integration_tests.sh
+++ cxx/integration_tests.sh
@@ -13,6 +13,6 @@ bazel build :hirm pclean:pclean tests:test_hirm_animals tests:test_irm_two_relat
 #./bazel-bin/hirm --mode=irm --iters=5 assets/animals.binary
 #./bazel-bin/hirm --seed=1 --iters=5 assets/animals.unary
 #./bazel-bin/hirm --iters=5 --load=assets/animals.unary.1.hirm assets/animals.unary
-#./bazel-bin/pclean/pclean --schema=assets/flights.schema --obs=assets/flights_dirty.100.csv --iters=5
-#./bazel-bin/pclean/pclean --schema=assets/hospitals.schema --obs=assets/hospital_dirty.100.csv --iters=5
+./bazel-bin/pclean/pclean --schema=assets/flights.schema --obs=assets/flights_dirty.100.csv --iters=5
+./bazel-bin/pclean/pclean --schema=assets/hospitals.schema --obs=assets/hospital_dirty.100.csv --iters=5
 ./bazel-bin/pclean/pclean --schema=assets/rents.schema --obs=assets/rents_dirty.100.csv --iters=5
