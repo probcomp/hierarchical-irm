6f0c401fd1d1b469b835c76e79305b001d1f6701
Thomas Colthurst <thomaswc@google.com>
2024-08-13 20:47:53 +0000

    Time the integration tests

--- cxx/integration_tests.sh
+++ cxx/integration_tests.sh
@@ -7,13 +7,22 @@ set -x
 
 # Run integration test suite
 bazel build tests:test_hirm_animals tests:test_irm_two_relations tests:test_misc
+startt1=$(date +%s)
 ./bazel-bin/tests/test_hirm_animals
 ./bazel-bin/tests/test_irm_two_relations
 ./bazel-bin/tests/test_misc
+endt1=$(date +%s)
 bazel build -c opt :hirm pclean:pclean
 ./bazel-bin/hirm --mode=irm --iters=5 assets/animals.binary
+startt2=$(date +%s)
 ./bazel-bin/hirm --seed=1 --iters=5 assets/animals.unary
 ./bazel-bin/hirm --iters=5 --load=assets/animals.unary.1.hirm assets/animals.unary
+endt2=$(date +%s)
+startt3=$(date +%s)
 #./bazel-bin/pclean/pclean --schema=assets/flights.schema --obs=assets/flights_dirty.100.csv --iters=5
 ./bazel-bin/pclean/pclean --schema=assets/hospitals.schema --obs=assets/hospital_dirty.10.csv --iters=5
 #./bazel-bin/pclean/pclean --schema=assets/rents.schema --obs=assets/rents_dirty.100.csv --iters=5
+endt3=$(date +%s)
+echo "Integration tests in /tests ran in $(($endt1-$startt1)) seconds"
+echo "hirm integration tests ran in $(($endt2-$startt2)) seconds"
+echo "pclean integration tests ran in $(($endt3-$startt3)) seconds"
