cb63a26d2a66c58d245495ccbab7a840db0bfe1b
Thomas Colthurst <thomaswc@google.com>
2024-09-17 18:53:14 +0000

    Add install.sh; fix some bugs

---
+++ py/install.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+# Install need Python modules:
+# matplotlib
+# numpy
+
+sudo apt-get update
+sudo apt-get install python3-matplotlib
+sudo apt-get install python3-numpy
--- py/visualize_unary.py
+++ py/visualize_unary.py
@@ -8,18 +8,25 @@ def main():
   parser = argparse.ArgumentParser(
       description="Generate matrix plots for a collection of unary relations")
   parser.add_argument(
-      "observations", type=str, required=True,
+      "--observations", required=True, type=str,
       help="Path to file containing observations")
   parser.add_argument(
-      "clusters", type=str, required=True,
+      "--clusters", required=True, type=str,
       help="Path to file containing HIRM output cluster")
   parser.add_argument(
-      "output", type=str, required=True,
-      help="Prefix to all output files")
+      "--output", required=True, type=str,
+      help="Path to write HTML output to.")
 
   args = parser.parse_args()
 
+  print(f"Loading observations from {args.observations} ...")
   obs = hirm_io.load_observations(args.observations)
-  clusters = hirm_io.load_observations(args.clusters)
+  print(f"Loading clusters from {args.clusters} ...")
+  clusters = hirm_io.load_clusters(args.clusters)
 
+  print(f"Writing html to {args.output} ...")
   visualize.plot_unary_matrices(clusters, obs, args.output)
+
+
+if __name__ == '__main__':
+  main()
