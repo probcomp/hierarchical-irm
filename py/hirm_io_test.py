46234cb5fcd7ae6924cf1def8398a73c319f9568
Thomas Colthurst <thomaswc@google.com>
2024-10-03 17:29:43 +0000

    Handle distribution parameters when parsing hirm schema from python

--- py/hirm_io_test.py
+++ py/hirm_io_test.py
@@ -22,6 +22,17 @@ class TestHirmIo(unittest.TestCase):
     self.assertEqual(relations["big"].distribution, "bernoulli")
     self.assertEqual(relations["big"].domains, ["animal"])
 
+  def test_load_schema_with_distribution_parameters(self):
+    relations = hirm_io.load_schema("../cxx/assets/pclean_rents_clean_ternary.schema")
+    self.assertEqual(len(relations), 3)
+    self.assertEqual(relations["has_type"].name, "has_type")
+    self.assertEqual(relations["has_type"].distribution, "stringcat")
+    self.assertEqual(len(relations["has_type"].parameters), 1)
+    print(relations["has_type"].parameters)
+    self.assertEqual(relations["has_type"].parameters["strings"],
+                     '"1br 2br 3br 4br studio"')
+    self.assertEqual(relations["has_type"].domains, ["record", "County", "State"])
+
   def test_load_observations(self):
     observations = hirm_io.load_observations("../cxx/assets/animals.unary.obs")
     self.assertEqual(len(observations), 4250)
@@ -31,15 +42,16 @@ class TestHirmIo(unittest.TestCase):
 
   def test_load_clusters(self):
     clusters = hirm_io.load_clusters("../cxx/assets/animals.unary.hirm")
-    self.assertEqual(len(clusters), 3)
-    self.assertEqual(clusters[0].cluster_id, "1")
-    self.assertEqual(clusters[1].cluster_id, "3")
-    self.assertEqual(clusters[2].cluster_id, "6")
-    self.assertEqual(clusters[0].relations[0], "paws")
+    self.assertEqual(len(clusters), 4)
+    self.assertEqual(clusters[0].cluster_id, "0")
+    self.assertEqual(clusters[1].cluster_id, "5")
+    self.assertEqual(clusters[2].cluster_id, "7")
+    self.assertEqual(clusters[3].cluster_id, "8")
+    self.assertEqual(clusters[0].relations[0], "quadrapedal")
     self.assertEqual(len(clusters[0].domain_clusters), 3)
     self.assertEqual(clusters[0].domain_clusters[0].cluster_id, "0")
     self.assertEqual(clusters[0].domain_clusters[0].domain, "animal")
-    self.assertEqual(clusters[0].domain_clusters[0].entities[0], "beaver")
+    self.assertEqual(clusters[0].domain_clusters[0].entities[0], "antelope")
 
 
 if __name__ == "__main__":
